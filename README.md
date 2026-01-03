525b9bcf6249bd60ab7d9d6377141157c0d0e0cb
Alister Burt <alisterburt@gmail.com>
2026-01-03 15:01:15 -0800

    Squashed 'packages/primitives/torch-image-interpolation/' content from commit 8ab5f7e
    git-subtree-dir: packages/primitives/torch-image-interpolation
    git-subtree-split: 8ab5f7e60766c682eff9d4ece616d9e1fcee53a6

---
+++ README.md
@@ -0,0 +1,136 @@
+# torch-image-interpolation
+
+[![License](https://img.shields.io/pypi/l/torch-image-interpolation.svg?color=green)](https://github.com/teamtomo/torch-image-interpolation/raw/main/LICENSE)
+[![PyPI](https://img.shields.io/pypi/v/torch-image-interpolation.svg?color=green)](https://pypi.org/project/torch-image-interpolation)
+[![Python Version](https://img.shields.io/pypi/pyversions/torch-image-interpolation.svg?color=green)](https://python.org)
+[![CI](https://github.com/teamtomo/torch-image-interpolation/actions/workflows/ci.yml/badge.svg)](https://github.com/teamtomo/torch-image-interpolation/actions/workflows/ci.yml)
+[![codecov](https://codecov.io/gh/teamtomo/torch-image-interpolation/branch/main/graph/badge.svg)](https://codecov.io/gh/teamtomo/torch-image-interpolation)
+
+1D/2D/3D image interpolation routines in PyTorch.
+
+## Why?
+
+This package provides a simple, consistent API for
+
+- sampling values from 1D/2D/3D images (`sample_image_1d()`/`sample_image_2d()`/`sample_image_3d()`)
+- inserting values into 2D/3D images (`insert_into_image_1d()`/`insert_into_image_2d()`/`insert_into_image_3d()`)
+
+Operations are differentiable, multichannel data and complex valued images are supported.
+
+[`torch.nn.functional.grid_sample`](https://pytorch.org/docs/stable/generated/torch.nn.functional.grid_sample.html)
+is used under the hood for sampling.
+
+# Installation
+
+```shell
+pip install torch-image-interpolation
+```
+
+# Usage
+
+## Coordinate System
+
+This library uses an array-like coordinate system where coordinate values span from `0`
+to `dim_size - 1` for each dimension.
+Fractional coordinates are supported and values are interpolated appropriately.
+
+### 1D Images
+
+For 1D images (vectors) with shape `(w, )` or `(c, w)` 
+coordinates are a `(...)` array of coordinates in the `w` dimension.
+
+For example, in a `(28, )` image, valid coordinates range from `0` to `27`.
+
+### 2D Images
+
+For 2D images with shape `(h, w)` or `(c, h, w)`:
+
+Coordinates are ordered as `[y, x]` where:
+
+- `y` is the position in the height dimension (first dimension of shape)
+- `x` is the position in the width dimension (second dimension of shape)
+
+For example, in a `(28, 28)` image, valid coordinates range from `[0, 0]` to `[27, 27]`
+
+### 3D Images
+
+For 3D images with shape `(d, h, w)` or `(c, d, h, w)`:
+
+Coordinates are ordered as `[z, y, x]` where:
+
+- `z` is the position in the depth dimension (first dimension of shape)
+- `y` is the position in the height dimension (second dimension of shape)
+- `x` is the position in the width dimension (third dimension of shape)
+
+For example, in a `(28, 28, 28)` volume, valid coordinates range from `[0, 0, 0]` to
+`[27, 27, 27]`.
+
+## Sample from image
+
+```python
+import torch
+import numpy as np
+from torch_image_interpolation import sample_image_2d
+
+# example (h, w) image
+image = torch.rand((28, 28))
+
+# make an arbitrary stack (..., 2) of 2d coords
+coords = torch.tensor(np.random.uniform(low=0, high=27, size=(6, 7, 8, 2))).float()
+
+# sampling returns a (6, 7, 8) array of samples
+# using bilinear interpolation (the default)
+sample_bilinear = sample_image_2d(image=image, coordinates=coords, interpolation='bilinear')
+
+# or a different interpolation mode...
+samples_nearest = sample_image_2d(image=image, coordinates=coords, interpolation='nearest')
+samples_bicubic = sample_image_2d(image=image, coordinates=coords, interpolation='bicubic')
+```
+
+The API is identical for 1D (`w`) or 3D `(d, h, w)` images but takes `(...)` or `(..., 3)` arrays of
+coordinates respectively.
+
+Sampling is supported for multichannel images in 1D `(c, w)`, 2D `(c, h, w)` and 3D `(c, d, h, w)`. 
+Sampling multichannel images returns `(..., c)` arrays of values.
+
+## Insert into image
+
+```python
+import torch
+import numpy as np
+from torch_image_interpolation import insert_into_image_2d
+
+# example (h, w) image
+image = torch.zeros((28, 28))
+
+# make an arbitrary stack (..., 2) of 2d coords
+coords = torch.tensor(np.random.uniform(low=0, high=27, size=(3, 4, 2)))
+
+# generate random values to place at coords
+values = torch.rand(size=(3, 4))
+
+# insert values into the image with bilinear interpolation (the default)
+image_bilinear, weights_bilinear = insert_into_image_2d(
+    values, image=image, coordinates=coords, interpolation='bilinear'
+)
+
+# you can specify a different interpolation mode
+image_nearest, weights_nearest = insert_into_image_2d(
+    values, image=image, coordinates=coords, interpolation='nearest'
+)
+```
+
+The API is identical for 1D `(w, )` and 3D `(d, h, w)` images but requires `(...)` and (..., 3)` arrays of
+coordinates respectively.
+
+Insertion is supported for multichannel images in 1D `(c, w)`, 2D `(c, h, w)` and 3D `(c, d, h, w)`. 
+Inserting into multichannel images requires `(..., c)` arrays of values.
+
+
+## Similar packages
+
+- https://github.com/balbasty/torch-interpol
+
+
+
+
---
+++ tests/test_image_interpolation_2d.py
@@ -0,0 +1,190 @@
+import einops
+import numpy as np
+import torch
+import pytest
+
+from torch_image_interpolation import sample_image_2d, insert_into_image_2d
+
+
+def test_sample_image_2d():
+    # basic sanity check only
+    image = torch.rand((28, 28))
+
+    # make an arbitrary stack (..., 2) of 2d coords
+    arbitrary_shape = (6, 7, 8)
+    coords = torch.tensor(np.random.randint(low=0, high=27, size=(*arbitrary_shape, 2)))
+
+    # sample
+    samples = sample_image_2d(image=image, coordinates=coords)
+    assert samples.shape == (6, 7, 8)
+
+
+def test_sample_image_2d_complex_input():
+    # basic sanity check only
+    image = torch.complex(real=torch.rand((28, 28)), imag=torch.rand(28, 28))
+
+    # make an arbitrary stack (..., 2) of 2d coords
+    arbitrary_shape = (6, 7, 8)
+    coords = torch.tensor(np.random.randint(low=0, high=27, size=(*arbitrary_shape, 2)))
+
+    # sample
+    samples = sample_image_2d(image=image, coordinates=coords)
+    assert samples.shape == arbitrary_shape
+
+
+def test_sample_multichannel_image_2d():
+    n_channels = 3
+    # basic sanity check only
+    image = torch.rand((n_channels, 28, 28))
+
+    # make an arbitrary stack (..., 2) of 2d coords
+    arbitrary_shape = (6, 7, 8)
+    coords = torch.tensor(np.random.randint(low=0, high=27, size=(*arbitrary_shape, 2)))
+
+    # sample
+    samples = sample_image_2d(image=image, coordinates=coords)
+    assert samples.shape == (*arbitrary_shape, n_channels)
+
+
+def test_sample_image_2d_multichannel_complex_input():
+    n_channels = 3
+
+    # basic sanity check only
+    image = torch.complex(real=torch.rand((28, 28)), imag=torch.rand(28, 28))
+    image = einops.repeat(image, 'h w -> c h w', c=n_channels)
+
+    # make an arbitrary stack (..., 2) of 2d coords
+    arbitrary_shape = (6, 7, 8)
+    coords = torch.tensor(np.random.randint(low=0, high=27, size=(*arbitrary_shape, 2)))
+
+    # sample
+    samples = sample_image_2d(image=image, coordinates=coords)
+    assert samples.shape == (*arbitrary_shape, n_channels)
+
+
+def test_insert_into_image_2d():
+    image = torch.zeros((28, 28)).float()
+
+    # single value
+    value = torch.tensor([5]).float()
+    coordinate = torch.tensor([10.5, 14.5]).view((1, 2))
+
+    # sample
+    image, weights = insert_into_image_2d(value, coordinates=coordinate, image=image, interpolation="bilinear")
+
+    # check value (5) is evenly split over 4 nearest pixels
+    expected = einops.repeat(torch.tensor([5 / 4]), '1 -> 2 2')
+    assert torch.allclose(image[10:12, 14:16], expected)
+
+    # check for zeros elsewhere
+    assert torch.allclose(image[:10, :14], torch.zeros_like(image[:10, :14]))
+
+
+def test_insert_into_image_2d_multiple():
+    image = torch.zeros((28, 28)).float()
+
+    # multiple values
+    values = torch.ones(size=(6, 7, 8)).float()
+    coordinates = torch.tensor(np.random.randint(low=0, high=27, size=(6, 7, 8, 2)))
+
+    # sample
+    image, weights = insert_into_image_2d(values, coordinates=coordinates, image=image, interpolation="bilinear")
+
+    # check for nonzero value at one point
+    sample_point = coordinates[0, 0, 0]
+    y, x = sample_point
+    assert image[y, x] > 0
+
+
+def test_insert_multiple_values_into_multichannel_image_2d_bilinear():
+    n_channels = 3
+    image = torch.zeros((n_channels, 28, 28)).float()
+
+    # multiple values
+    arbitrary_shape = (6, 7, 8)
+    values = torch.ones(size=(*arbitrary_shape, n_channels)).float()
+    coordinates = torch.tensor(np.random.randint(low=0, high=27, size=(*arbitrary_shape, 2)))
+
+    # sample
+    image, weights = insert_into_image_2d(values, coordinates=coordinates, image=image, interpolation="bilinear")
+
+    # check for nonzero value at one point
+    sample_point = coordinates[0, 0, 0]
+    y, x = sample_point
+    assert image[0, y, x] > 0
+
+    # check output shapes
+    assert image.shape == (n_channels, 28, 28)
+    assert weights.shape == (28, 28)
+
+
+def test_insert_multiple_values_into_multichannel_image_2d_nearest():
+    n_channels = 3
+    image = torch.zeros((n_channels, 28, 28)).float()
+
+    # multiple values
+    arbitrary_shape = (6, 7, 8)
+    values = torch.ones(size=(*arbitrary_shape, n_channels)).float()
+    coordinates = torch.tensor(np.random.randint(low=0, high=27, size=(*arbitrary_shape, 2)))
+
+    # sample
+    image, weights = insert_into_image_2d(values, coordinates=coordinates, image=image, interpolation="nearest")
+
+    # check for nonzero value at one point
+    sample_point = coordinates[0, 0, 0]
+    y, x = sample_point
+    assert image[0, y, x] > 0
+
+    # check output shapes
+    assert image.shape == (n_channels, 28, 28)
+    assert weights.shape == (28, 28)
+
+
+def test_insert_into_image_nearest_interp_2d():
+    image = torch.zeros((28, 28)).float()
+
+    # single value
+    value = torch.tensor([5]).float()
+    coordinate = torch.tensor([10.7, 14.3]).view((1, 2))
+
+    # sample
+    image, weights = insert_into_image_2d(value, coordinates=coordinate, image=image, interpolation='nearest')
+
+    # check value (5) is added at nearest pixel
+    expected = torch.zeros((28, 28)).float()
+    expected[11, 14] = 5
+    assert torch.allclose(image, expected)
+
+
+@pytest.mark.parametrize(
+    "dtype",
+    [torch.float32, torch.float64, torch.complex64, torch.complex128]
+)
+def test_insert_into_image_2d_type_consistency(dtype):
+    image = torch.rand((4, 4), dtype=dtype)
+    coords = torch.tensor(np.random.uniform(low=0, high=3, size=(3, 4, 2)))
+    values = torch.rand(size=(3, 4), dtype=dtype)
+    weights = torch.zeros_like(image, dtype=torch.float64)
+
+    for mode in ['bilinear', 'nearest']:
+        image, weights = insert_into_image_2d(
+            values,
+            image=image,
+            weights=weights,
+            coordinates=coords,
+            interpolation=mode,
+        )
+        assert image.dtype == dtype
+        assert weights.dtype == torch.float64
+
+
+def test_insert_into_image_3d_type_error():
+    image = torch.rand((4, 4), dtype=torch.complex64)
+    coords = torch.tensor(np.random.uniform(low=0, high=3, size=(3, 4, 2)))
+    values = torch.rand(size=(3, 4), dtype=torch.complex128)
+    with pytest.raises(ValueError):
+        insert_into_image_2d(
+            values,
+            image=image,
+            coordinates=coords,
+        )
